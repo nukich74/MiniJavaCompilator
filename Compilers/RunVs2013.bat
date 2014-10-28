59a631faab8a45b15d671554950b59714fff2b66
Vladislav Vorotilov <vlad.vorotilov@gmail.com>
2014-10-28 17:55:14 +0300

    added win_flex_bison

--- Compilers/RunVs2013.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Добавляем путь к GnuWin32 в PATH
-set GNUWIN32=C:\GnuWin32
-set PATH=%PATH%;%GNUWIN32%\bin
-
-echo Запускаем студию (можно прописать и сам solution)
-start devenv.exe Compilers.sln
-rem start Compilers.sln
