5486847645942365864b248c366504c588156bf9
MysinYurii <misin.yurii@phystech.edu>
2014-11-11 18:13:10 +0400

    Не смотреть в ExpList

--- Compilers/Compilers/ExpList.h
+++ Compilers/Compilers/ExpList.h
@@ -1,5 +1,20 @@
+#pragma once
+
+#include "Visitor.h"
 #include "Grammar.h"
+#include <memory>
 
 class CExp : public IExpList {
-
+public:
+	CExp(IExp* _exp) : exp(_exp) {}
+	void accept(IVisitor& visitor) { visitor.visit(*this); }
+	std::shared_ptr<IExp> exp;
 };
+
+class CExpExpRest : public IExpList {
+	CExpExpRest(IExpList* _expRest, IExp* _exp) : exp(_exp), expRest(_expRest) {}
+	void accept(IVisitor& visitor) { visitor.visit(*this); }
+	std::shared_ptr<IExpList> expRest;
+	std::shared_ptr<IExp> exp;
+};
+
--- Compilers/Compilers/Visitor.h
+++ Compilers/Compilers/Visitor.h
@@ -9,22 +9,34 @@
 #include "MethodDeclList.h"
 #include "FormalList.h"
 #include "Exp.h"
+#include "ExpList.h"
 
 class IVisitor {
 public:
+	// EXP -> EXP BINOP EXP
 	virtual void visit( CExpBinOpExp& exp );
+	// EXP -> -EXP
 	virtual void visit( CUnMinExp& exp );
+	// EXP -> EXP [ EXP ]
 	virtual void visit( CExpWithIndex& exp );
+	// EXP -> EXP.LENGTH
 	virtual void visit( CExpDotLength& exp );
+	// EXP -> EXP . ID ( EXPLIST )
 	virtual void visit( CExpIdExpList& exp );
+	// EXP -> EXP . ID ( )
 	virtual void visit( CExpIdVoidExpList& exp );
+	// EXP -> INTEGER_LITERAL
 	virtual void visit( CIntegerLiteral& exp );
+	// EXP -> TRUE
 	virtual void visit( CTrue& exp );
+	// EXP -> FALSE
 	virtual void visit( CFalse& exp );
+	// EXP -> ID
 	virtual void visit( CId& exp );
+	// EXP -> THIS
 	virtual void visit( CThis& exp );
 	virtual void visit( CNewIntExpIndex& exp );
-	virtual void visit( CNewIdExpIndex& exp );
+	virtual void visit( CNewId& exp );
 	virtual void visit( CNotExp& exp );
 	virtual void visit( CExpInBrackets& exp );
 	virtual void visit( CProgram& program );
@@ -35,4 +47,6 @@ public:
 	virtual void visit( CVarDecl& varDecl );
 	virtual void visit( CMethodDeclList& methodDeclList );
 	virtual void visit( CFormalList& formalList );
+	virtual void visit( CExp& expList );
+	virtual void visit( CExpExpRest& expList );
 };
