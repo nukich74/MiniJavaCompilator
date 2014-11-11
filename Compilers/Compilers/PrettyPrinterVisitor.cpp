f1700c6681ed5ab878da8a515c841c0164314847
Vladislav Vorotilov <vlad.vorotilov@gmail.com>
2014-11-11 17:46:03 +0300

    Добавлен PrettyPrinter

---
+++ Compilers/Compilers/PrettyPrinterVisitor.cpp
@@ -0,0 +1,111 @@
+#include "PrettyPrinterVisitor.h"
+
+void CPrettyPrinterVisitor::visit( CExpBinOpExp& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CUnMinExp& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CExpWithIndex& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CExpDotLength& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CExpIdExpList& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CExpIdVoidExpList& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CIntegerLiteral& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CTrue& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CFalse& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CId& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CThis& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CNewIntExpIndex& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CNewIdExpIndex& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CNotExp& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CExpInBrackets& exp )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CProgram& program )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CMainClass& mainClass )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CClassDeclList& classDeclList )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CClassDecl& classDecl )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CVarDeclList& varDeclList )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CVarDecl& varDecl )
+{
+
+}
+
+void CPrettyPrinterVisitor::visit( CMethodDeclList& methodDeclList )
+{
+
+}
---
+++ Compilers/Compilers/PrettyPrinterVisitor.h
@@ -0,0 +1,27 @@
+#include "Visitor.h"
+
+class CPrettyPrinterVisitor : public IVisitor {
+public:
+	virtual void visit( CExpBinOpExp& exp );
+	virtual void visit( CUnMinExp& exp );
+	virtual void visit( CExpWithIndex& exp );
+	virtual void visit( CExpDotLength& exp );
+	virtual void visit( CExpIdExpList& exp );
+	virtual void visit( CExpIdVoidExpList& exp );
+	virtual void visit( CIntegerLiteral& exp );
+	virtual void visit( CTrue& exp );
+	virtual void visit( CFalse& exp );
+	virtual void visit( CId& exp );
+	virtual void visit( CThis& exp );
+	virtual void visit( CNewIntExpIndex& exp );
+	virtual void visit( CNewIdExpIndex& exp );
+	virtual void visit( CNotExp& exp );
+	virtual void visit( CExpInBrackets& exp );
+	virtual void visit( CProgram& program );
+	virtual void visit( CMainClass& mainClass );
+	virtual void visit( CClassDeclList& classDeclList );
+	virtual void visit( CClassDecl& classDecl );
+	virtual void visit( CVarDeclList& varDeclList );
+	virtual void visit( CVarDecl& varDecl );
+	virtual void visit( CMethodDeclList& methodDeclList );
+};
