f8109fe437e600d4cbe976be0a85114b08ced53f
MysinYurii <misin.yurii@phystech.edu>
2014-11-11 18:39:49 +0400

    ExpList

--- Compilers/Compilers/ExpList.h
+++ Compilers/Compilers/ExpList.h
@@ -3,5 +3,16 @@
 #include "Visitor.h"
 #include "Grammar.h"
 #include <memory>
+#include <list>
 
+class CExpList : public IExpList {
+public:
+	CExpList(CExpList* _restOfList, IExp* _exp) {
+		if (_restOfList != 0) {
+			expList = _restOfList->expList;
+		}
+		expList.push_back( std::shared_ptr<IExp>(_exp) );
+	}
 
+	std::list<std::shared_ptr<IExp>> expList;
+};
