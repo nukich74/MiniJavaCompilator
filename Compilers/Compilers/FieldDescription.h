85e7c69ef4068cbdea23dc4b5de4058f99d9d334
FrolovNikolay <frolov.nikolay@phystech.edu>
2014-11-11 18:24:40 +0400

    Заготовка для визитора, строящего таблицу символов.

---
+++ Compilers/Compilers/FieldDescription.h
@@ -0,0 +1,15 @@
+﻿// Автор: Николай Фролов
+
+// Описание: Класс, аггрегирующий информацию о переменной - ее имени и типе.
+
+#include <string>
+#include "TypeIdentifier.h"
+
+struct CFieldDescription {
+public:
+	// Имя переменной.
+	std::string Name;
+	// Тип переменной.
+	CTypeIdentifier Type;
+
+};
---
+++ Compilers/Compilers/SymbolTable.cpp
@@ -0,0 +1,10 @@
+﻿// Автор: Николай Фролов
+
+#include "SymbolTable.h"
+
+void CSymbolTable::AddClass( const CClassDescription& newClass ) 
+{ 
+	if( Classes.find( newClass.Name ) != Classes.end() ) {
+		Classes.insert( std::pair< std::string, CClassDescription>( newClass.Name, newClass ) );
+	}
+}
