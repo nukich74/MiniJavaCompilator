ead34a8e38a388d93b94967f5deaa8a4dbe0b765
FrolovNikolay <frolov.nikolay@phystech.edu>
2014-12-09 02:30:44 +0300

    TypeChecker начало.

---
+++ Compilers/Compilers/SymbolsTable/TypeChecker.cpp
@@ -0,0 +1,98 @@
+﻿// Автор: Фролов Николай.
+
+#include <TypeChecker.h>
+#include <common.h>
+
+using namespace SymbolsTable;
+
+void CTypeChecker::Visit( const CProgram& program )
+{
+	program.MainClass()->Accept( *this );
+	program.ClassDeclList()->Accept( *this );
+}
+
+void CTypeChecker::Visit( const CMainClass& mainClass )
+{
+	// currentClass = &( symbolsTable.Classes()[mainClass.MainClassName()] );
+	currentMethod = &currentClass->Methods[0];
+	mainClass.StatementList()->Accept( *this );
+	currentMethod = 0;
+	currentClass = 0;
+}
+
+void CTypeChecker::Visit( const CStatementList& statementList )
+{
+	for( auto& statement : statementList.StatmentList() ) {
+		statement->Accept( *this );
+	}
+}
+
+void CTypeChecker::Visit( const CAssignStatement& assignStatement )
+{
+	if( assignStatement.IndexExp() == 0 ) {
+		if( !setLastTypeByIdentifier( assignStatement.LeftId() ) ) {
+			// Ошибка - правая часть неопределена.
+			return;
+		}
+		CTypeIdentifier leftType = lastType;
+		assignStatement.RightExp()->Accept( *this );
+		CTypeIdentifier rightType = lastType;
+		if( leftType != rightType ) {
+			// Ошибка - разные типы в равенстве.
+			return;
+		}
+	} else {
+		assignStatement.IndexExp()->Accept( *this );
+		if( lastType != BT_IntArr ) {
+			// Ошибка - индекс не является целочисленным.
+			return;
+		}
+		if( !setLastTypeByIdentifier( assignStatement.LeftId() ) ) {
+			// Ошибка - правая часть не определена.
+			return;
+		}
+		CTypeIdentifier leftType = lastType;
+		if( leftType != BT_IntArr ) {
+			// Ошибка - пытаемся индексировать не массив int.
+			return;
+		}
+		assignStatement.RightExp()->Accept( *this );
+		if( lastType != BT_Int ) {
+			// Ошибка - пытаемся элементу массива int присвоить не int.
+		}
+	}
+}
+
+void CTypeChecker::Visit( const CClassDeclList& classDeclList )
+{
+	for( auto& classDecl : classDeclList.ClassDeclList() ) {
+		classDecl->Accept( *this );
+	}
+}
+
+bool CTypeChecker::setLastTypeByIdentifier( const std::string& id ) const
+{
+	if( currentMethod != 0 ) {
+		for( auto& localVariable : currentMethod->Locals ) {
+			if( localVariable.Name() == id ) {
+				lastType = localVariable.Type;
+				return true;
+			}
+		}
+		for( auto& paramVariable : currentMethod->Params ) {
+			if( paramVariable.Name() == id ) {
+				lastType = paramVariable.Type;
+				return true;
+			}
+		}
+	}
+	if( currentClass != 0 ) {
+		for( auto& fieldVariable : currentClass->Fields ) {
+			if( fieldVariable.Name() == id ) {
+				lastType = fieldVariable.Type;
+				return true;
+			}
+		}
+	}
+	return false;
+}
---
+++ Compilers/Compilers/SymbolsTable/TypeChecker.h
@@ -0,0 +1,58 @@
+﻿// Автор: Фролов Николай.
+
+// Описание: Визитор проверяющий AST на наличие семантических ошибок.
+
+#pragma once
+
+#include <Visitor.h>
+#include <ClassDescriptor.h>
+#include <TypeIdentifier.h>
+#include <SymbolsTable.h>
+
+class CTypeChecker : public IVisitor {
+public:
+	CTypeChecker( const SymbolsTable::CSymbolsTable& _symbolsTable )
+		: symbolsTable( _symbolsTable )
+	{ }
+
+	virtual void Visit( const CExpBinOpExp& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CUnMinExp& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CExpWithIndex& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CExpDotLength& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CExpIdExpList& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CExpIdVoidExpList& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CIntegerLiteral& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CTrue& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CFalse& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CId& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CThis& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CNewIntExpIndex& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CNewId& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CNotExp& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CExpInBrackets& exp ) { IVisitor::Visit( exp ); }
+	virtual void Visit( const CProgram& program );
+	virtual void Visit( const CMainClass& mainClass );
+	virtual void Visit( const CClassDeclList& classDeclList );
+	virtual void Visit( const CClassDecl& classDecl );
+	virtual void Visit( const CVarDeclList& varDeclList );
+	virtual void Visit( const CVarDecl& varDecl );
+	virtual void Visit( const CMethodDeclList& methodDeclList );
+	virtual void Visit( const CMethodDecl& methodDecl );
+	virtual void Visit( const CFormalList& formalList );
+	virtual void Visit( const CType& type );
+	virtual void Visit( const CStatementList& statementList );
+	virtual void Visit( const CAssignStatement& assignStatement );
+	virtual void Visit( const CPrintStatement& printStatement ) { IVisitor::Visit( printStatement ); }
+	virtual void Visit( const CCurlyBraceStatement& curlyBraceStatement ) { IVisitor::Visit( curlyBraceStatement ); }
+	virtual void Visit( const CIfStatement& ifStatement ) { IVisitor::Visit( ifStatement ); }
+	virtual void Visit( const CWhileStatement& whileStatement ) { IVisitor::Visit( whileStatement ); }
+	virtual void Visit( const CExpList& expList ) { IVisitor::Visit( expList ); }
+
+private:
+	const SymbolsTable::CSymbolsTable& symbolsTable;
+	const SymbolsTable::CClassDescriptor* currentClass;
+	const SymbolsTable::CMethodDescriptor* currentMethod;
+	mutable SymbolsTable::CTypeIdentifier lastType;
+
+	bool setLastTypeByIdentifier( const std::string& id ) const;
+};
