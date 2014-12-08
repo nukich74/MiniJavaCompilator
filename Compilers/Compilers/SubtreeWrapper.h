6ccf43a7e6781e3b1888b28b976eba167191e7f3
MysinYurii <misin.yurii@phystech.edu>
2014-12-08 23:11:02 +0300

    IRSubreeWrapper

--- Compilers/Compilers/SubtreeWrapper.h
+++ Compilers/Compilers/SubtreeWrapper.h
@@ -1,8 +1,17 @@
 // Автор: Воротилов Владислав
 
 #pragma once
+#include "IRTree.h"
+#include "Temp.h"
 
 namespace Translate {
+	class ISubtreeWrapper {
+	public:
+		virtual const IRTree::IIRExp* ToExp( ) const = 0;
+		virtual const IRTree::IIRStm* ToStm( ) const = 0;
+		virtual const IRTree::IIRStm* ToConditional( const Temp::CLabel* t, const Temp::CLabel* f )const = 0;
+
+	};
 
 
 }; //namespace Translate
