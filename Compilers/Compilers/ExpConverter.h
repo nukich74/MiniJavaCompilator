49dabc22ccd46d06c217f99ca475c06dfb145c6e
Vladislav Vorotilov <vlad.vorotilov@gmail.com>
2014-12-09 18:48:57 +0400

    правки

--- Compilers/Compilers/ExpConverter.h
+++ Compilers/Compilers/ExpConverter.h
@@ -10,7 +10,7 @@ namespace Translate {
 		const IRTree::IIRExp* ToExp() { return exp; }
 		const IRTree::IIRStm* ToStm() { return new IRTree::CIRExp(exp); }
 		const IRTree::IIRStm* ToConditional( const Temp::CLabel* t, const Temp::CLabel* f ) { 
-			return new IRTree::CIRCjump(IRTree::CJ_NotEqual, exp, const_cast<const IRTree::CIRConst*>(new IRTree::CIRConst(0)), t, f); 
+			return new IRTree::CIRCjump( IRTree::CJ_NotEqual, exp, new IRTree::CIRConst( 0 ), t, f ); 
 		}
 	private:
 		const IRTree::IIRExp* exp;
