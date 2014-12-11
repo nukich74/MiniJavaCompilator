a5a3a69ec3f743e46d296c391827ad915bf7f351
MysinYurii <misin.yurii@phystech.edu>
2014-12-11 18:56:16 +0300

    Wrapper

--- Compilers/Compilers/SubtreeWrapper.h
+++ Compilers/Compilers/SubtreeWrapper.h
@@ -7,8 +7,8 @@
 namespace Translate {
 	class ISubtreeWrapper {
 	public:
-		virtual const IRTree::IIRExp* ToExp( ) const = 0;
-		virtual const IRTree::IIRStm* ToStm( ) const = 0;
+		virtual const IRTree::IIRExp* ToExp() const = 0;
+		virtual const IRTree::IIRStm* ToStm() const = 0;
 		virtual const IRTree::IIRStm* ToConditional( const Temp::CLabel* t, const Temp::CLabel* f ) const = 0;
 
 	};
