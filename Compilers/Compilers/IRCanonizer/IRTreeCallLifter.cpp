5f697af8b42cebe9c520af43e6c8b5243c2d9b14
FrolovNikolay <frolov.nikolay@phystech.edu>
2015-03-16 15:56:59 +0400

    Скелет приобретает немного мяса.

--- Compilers/Compilers/IRCanonizer/IRTreeCallLifter.cpp
+++ Compilers/Compilers/IRCanonizer/IRTreeCallLifter.cpp
@@ -6,10 +6,13 @@ using namespace IRTree;
 
 void CIRTreeCallLifter::Visit( const CMove* node )
 {
+	node->dst->Accept( *this );
+	node->src->Accept( *this );
 }
 
 void CIRTreeCallLifter::Visit( const CExp* node )
 {
+	node->exp->Accept( *this );
 }
 
 void CIRTreeCallLifter::Visit( const CJump* node )
@@ -18,10 +21,14 @@ void CIRTreeCallLifter::Visit( const CJump* node )
 
 void CIRTreeCallLifter::Visit( const CCjump* node )
 {
+	node->left->Accept( *this );
+	node->right->Accept( *this );
 }
 
 void CIRTreeCallLifter::Visit( const CSeq* node )
 {
+	node->left->Accept( *this );
+	node->right->Accept( *this );
 }
 
 void CIRTreeCallLifter::Visit( const CConst* node )
@@ -38,22 +45,30 @@ void CIRTreeCallLifter::Visit( const CTemp* node )
 
 void CIRTreeCallLifter::Visit( const CBinop* node )
 {
+	node->left->Accept( *this );
+	node->right->Accept( *this );
 }
 
 void CIRTreeCallLifter::Visit( const CMem* node )
 {
+	node->exp->Accept( *this );
 }
 
 void CIRTreeCallLifter::Visit( const CCall* node )
 {
+	// Тут супервершинка.
 }
 
 void CIRTreeCallLifter::Visit( const CEseq* node )
 {
+	node->exp->Accept( *this );
+	node->stm->Accept( *this );
 }
 
 void CIRTreeCallLifter::Visit( const CExpList* node )
 {
+	node->head->Accept( *this );
+	node->tail->Accept( *this );
 }
 
 void CIRTreeCallLifter::Visit( const CLabel* node )
