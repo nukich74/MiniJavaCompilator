d59ac855ea7d145deb9ec225909f381d3f0461b3
Azat <davletshin.azat@phystech.edu>
2015-04-20 15:23:45 +0300

    Заготовка munchStm для Влада
    Нужно реализовать другие стейтменты

--- Compilers/Compilers/CodeGeneration/InstructionMuncher.cpp
+++ Compilers/Compilers/CodeGeneration/InstructionMuncher.cpp
@@ -1,6 +1,7 @@
 ﻿// Автор: Николай Фролов.
 
 #include "InstructionsMuncher.h"
+#include <IRTree\IRStm.h>
 
 using namespace CodeGeneration;
 
@@ -9,4 +10,17 @@ void CInstructionsMuncher::CodeGen()
 	for( auto& stm : reorderedStmList ) {
 		munchStm( stm.get() );
 	}
+}
+
+void CInstructionsMuncher::munchStm( const IRTree::IStm* stm )
+{
+	// MOVE(...)
+	const IRTree::CMove* asMove = dynamic_cast<const IRTree::CMove*>(stm);
+	if( asMove != 0 ) {
+		const IRTree::CMem* dstAsMem = dynamic_cast<const IRTree::CMem*>(asMove->dst.get());
+		assert( dstAsMem != 0 );
+		return munchMove( dstAsMem, asMove->src.get() );
+	}
+
+	// Перебор других случаев (TODO Влад)
 }
