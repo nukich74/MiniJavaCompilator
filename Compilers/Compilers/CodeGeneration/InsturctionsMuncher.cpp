6c9d1f251cbc167f1ed2c598fff36833aa315ceb
Vladislav Vorotilov <vlad.vorotilov@gmail.com>
2015-04-26 18:44:43 +0300

    Доделал stm свою часть

--- Compilers/Compilers/CodeGeneration/InsturctionsMuncher.cpp
+++ Compilers/Compilers/CodeGeneration/InsturctionsMuncher.cpp
@@ -42,6 +42,49 @@ void CInstructionsMuncher::munchStm( const IRTree::IStm* stm )
 	if( asJump != nullptr ) {
 		emit( new CodeGeneration::COper( std::string( "jmp l0" ), nullptr, nullptr, new std::list<Temp::CLabel>( 1, *asJump->label ) ) );
 	}
+
+	// Exp (тот который забывает про результат)
+	const IRTree::CExp* asExp = dynamic_cast<const IRTree::CExp*>( stm );
+	if( asExp != nullptr ) {
+		munchExp( asExp->exp.get() );
+	}
+
+	// CJump
+	const IRTree::CCjump* asCjump = dynamic_cast<const IRTree::CCjump*>(stm);
+	if( asCjump != nullptr ) {
+		auto left = munchExp( asCjump->left.get() );
+		auto right = munchExp( asCjump->right.get() );
+		auto cmp = new std::list<Temp::CTemp*>( );
+		auto labelList = new std::list<Temp::CLabel>();
+		labelList->push_back( *(asCjump->iftrue) );
+		cmp->push_back( left );
+		cmp->push_back( right );
+		emit( new CodeGeneration::COper( std::string( "cmp s0 s1" ), nullptr, cmp, nullptr ) );
+		switch( asCjump->relop ) {
+		case IRTree::CJ_Less:
+			emit( new CodeGeneration::COper( std::string( "jl l0" ), nullptr, nullptr, labelList ) );
+			break;
+		case IRTree::CJ_Greater:
+			emit( new CodeGeneration::COper( std::string( "jg l0" ), nullptr, nullptr, labelList ) );
+			break;
+		case IRTree::CJ_EqLess:
+			emit( new CodeGeneration::COper( std::string( "jle l0" ), nullptr, nullptr, labelList ) );
+			break;
+		case IRTree::CJ_EqGreater:
+			emit( new CodeGeneration::COper( std::string( "jge l0" ), nullptr, nullptr, labelList ) );
+			break;
+		case IRTree::CJ_Equal:
+			emit( new CodeGeneration::COper( std::string( "je l0" ), nullptr, nullptr, labelList ) );
+			break;
+		case IRTree::CJ_NotEqual:
+			emit( new CodeGeneration::COper( std::string( "jne l0" ), nullptr, nullptr, labelList ) );
+			break;
+		default:
+			assert( false );
+			break;
+		}
+	}
+
 }
 
 void CInstructionsMuncher::munchMove( const IRTree::CMem* dst, const IRTree::IExp* src )
@@ -51,5 +94,5 @@ void CInstructionsMuncher::munchMove( const IRTree::CMem* dst, const IRTree::IEx
 
 void CInstructionsMuncher::emit( IInstruction* instruction )
 {
-	assert( false );
+	instructionsList.emplace_back( std::unique_ptr<IInstruction>( instruction ) );
 }
