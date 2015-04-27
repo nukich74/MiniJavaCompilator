40b2152668d27af2be59574067e47446951f800b
FrolovNikolay <frolov.nikolay@phystech.edu>
2015-04-27 15:32:38 +0400

    MunchExp апдейт

--- Compilers/Compilers/CodeGeneration/InstructionsMuncherMunchExp.cpp
+++ Compilers/Compilers/CodeGeneration/InstructionsMuncherMunchExp.cpp
@@ -16,7 +16,7 @@ Temp::CTemp CInstructionsMuncher::munchExp( const IRTree::IExp* exp )
 	} else if( dynamic_cast<const IRTree::CConst*>( exp ) != 0 ) {
 		Temp::CTemp tmp;
 		int constValue = dynamic_cast<const IRTree::CConst*>( exp )->value;
-		emit( new COper( "MOVE d0 <- " + std::to_string( constValue ) + "\n", std::list<Temp::CTemp>( 1, tmp ), std::list<Temp::CTemp>() ) );
+		emit( new COper( "mov 'd0, " + std::to_string( constValue ), std::list<Temp::CTemp>( 1, tmp ), std::list<Temp::CTemp>() ) );
 		return tmp;
 	} else if( dynamic_cast<const IRTree::CTemp*>( exp ) != 0 ) {
 		return Temp::CTemp( dynamic_cast<const IRTree::CTemp*>( exp )->temp );
@@ -30,110 +30,127 @@ Temp::CTemp CInstructionsMuncher::munchExpMem( const IRTree::CMem* exp )
 {
 	if( dynamic_cast<const IRTree::CBinop*>( exp->exp.get() ) != 0 ) {
 		return munchExpBinopInMem( dynamic_cast<const IRTree::CBinop*>( exp->exp.get() ) );
-	} else if( dynamic_cast<const IRTree::CConst*>( exp->exp.get() ) != 0 ) {
-		Temp::CTemp tmp;
-		emit( new COper( "MOVE d0 <- M[i]\n", std::list<Temp::CTemp>( 1, tmp ), std::list<Temp::CTemp>() ) );
-		return tmp;
 	} else {
-		Temp::CTemp tmp;
-		emit( new COper( "MOVE d0 <- M[s0]\n", std::list<Temp::CTemp>( 1, tmp ),
-			std::list<Temp::CTemp>( 1, munchExp( exp->exp.get() ) ) ) );
-		return tmp;
+		// Кладем exp из mem во временную переменную.
+		Temp::CTemp tmp1;
+		emit( new COper( "mov 'd0, 's0", std::list<Temp::CTemp>( 1, tmp1 ), std::list<Temp::CTemp>( 1, munchExp( exp->exp.get() ) ) ) );
+		// Кладем framePointer во временную переменную.
+		Temp::CTemp tmp2;
+		emit( new COper( "mov 'd0, 's0", std::list<Temp::CTemp>( 1, tmp2 ), std::list<Temp::CTemp>( 1, *frame->FramePointer() ) ) );
+		// Кладем во временную переменную значение по заданному адресу.
+		Temp::CTemp tmp3;
+		std::list<Temp::CTemp> source = std::list<Temp::CTemp>( 1, tmp1 );
+		source.push_back( tmp2 );
+		emit( new COper( "mov 'd0, ['s0 + 's1]", std::list<Temp::CTemp>( 1, tmp3 ),
+			source ) );
+		return tmp3;
 	}
 }
 
 Temp::CTemp CInstructionsMuncher::munchExpBinopInMem( const IRTree::CBinop* exp )
 {
-	std::string command = "MOVE";
+	std::string command = "mov";
 	std::string binopOperation;
-	switch( exp->binop ) {
-		case IRTree::B_Plus:
-		case IRTree::B_Minus:
-			binopOperation = IRTree::ToString( exp->binop );
-			break;
-		default:
-			// смещение только по + и -.
-			assert( false );
+	if( exp->binop == IRTree::B_Plus || exp->binop == IRTree::B_Minus ) {
+		binopOperation = IRTree::ToString( exp->binop );
+	} else {
+		// Если в binop в mem не + и не -, то значит затесалась ошибка.
+		assert( false );
 	}
 	if( dynamic_cast<const IRTree::CConst*>( exp->left.get() ) != 0 && dynamic_cast<const IRTree::CConst*>( exp->right.get() ) != 0 ) {
-		int leftValue = dynamic_cast<const IRTree::CConst*>( exp->left.get() )->value;
-		int rightValue = dynamic_cast<const IRTree::CConst*>( exp->right.get() )->value;
-		Temp::CTemp tmp;
-		emit( new COper( command + " d0 <- M[" + std::to_string( leftValue ) + binopOperation + std::to_string( rightValue ) + "]\n",
-			std::list<Temp::CTemp>( 1, tmp ), std::list<Temp::CTemp>( ) ) );
-		return tmp;
+		// Если запрашиваем адрес по двум binop от двух констант - то что-то не так.
+		assert( false );
+		return Temp::CTemp();
 	} else if( dynamic_cast<const IRTree::CConst*>( exp->left.get() ) != 0 ) {
 		int leftValue = dynamic_cast<const IRTree::CConst*>( exp->left.get() )->value;
-		Temp::CTemp tmp;
-		emit( new COper( command + " d0 <- M[" + std::to_string( leftValue ) + binopOperation + "s0]\n",
-			std::list<Temp::CTemp>( 1, tmp ), std::list<Temp::CTemp>( 1, munchExp( exp->right.get() ) ) ) );
-		return tmp;
+		Temp::CTemp tmp1;
+		emit( new COper( "mov 'd0, 's0", std::list<Temp::CTemp>( 1, tmp1 ), std::list<Temp::CTemp>( 1, munchExp( exp->right.get() ) ) ) );
+		Temp::CTemp tmp2;
+		emit( new COper( "mov 'd0, 's0", std::list<Temp::CTemp>( 1, tmp2 ), std::list<Temp::CTemp>( 1, *frame->FramePointer() ) ) );
+		Temp::CTemp tmp3;
+		std::list<Temp::CTemp> source = std::list<Temp::CTemp>( 1, tmp2 );
+		source.push_back( tmp1 );
+		emit( new COper( command + " 'd0, ['s0 + " + std::to_string( leftValue ) + binopOperation + "'s1]",
+			std::list<Temp::CTemp>( 1, tmp3 ), source ) );
+		return tmp3;
 	} else if( dynamic_cast<const IRTree::CConst*>( exp->right.get() ) != 0 ) {
 		int rightValue = dynamic_cast<const IRTree::CConst*>( exp->right.get() )->value;
-		Temp::CTemp tmp;
-		emit( new COper( command + " d0 <- M[s0"+ binopOperation + std::to_string( rightValue ) + "]\n",
-			std::list<Temp::CTemp>( 1, tmp ), std::list<Temp::CTemp>( 1, munchExp( exp->left.get() ) ) ) );
-		return tmp;
+		Temp::CTemp tmp1;
+		emit( new COper( "mov 'd0, 's0", std::list<Temp::CTemp>( 1, tmp1 ), std::list<Temp::CTemp>( 1, munchExp( exp->left.get() ) ) ) );
+		Temp::CTemp tmp2;
+		emit( new COper( "mov 'd0, 's0", std::list<Temp::CTemp>( 1, tmp2 ), std::list<Temp::CTemp>( 1, *frame->FramePointer() ) ) );
+		Temp::CTemp tmp3;
+		std::list<Temp::CTemp> source = std::list<Temp::CTemp>( 1, tmp2 );
+		source.push_back( tmp1 );
+		emit( new COper( command + " 'd0, ['s0 + 's1 " + binopOperation + std::to_string( rightValue ) + "]",
+			std::list<Temp::CTemp>( 1, tmp3 ), source ) );
+		return tmp3;
 	} else {
-		Temp::CTemp tmp;
-		std::list<Temp::CTemp> src( 1, munchExp( exp->left.get() ) );
-		src.push_back( munchExp( exp->right.get() ) ); 
-		emit( new COper( command + " d0 <- M[s0" + binopOperation + "s1]\n",
-			std::list<Temp::CTemp>( 1, tmp ), src ) );
-		return tmp;
+		Temp::CTemp tmp1;
+		emit( new COper( "mov 'd0, 's0", std::list<Temp::CTemp>( 1, tmp1 ), std::list<Temp::CTemp>( 1, munchExp( exp->left.get() ) ) ) );
+		Temp::CTemp tmp2;
+		emit( new COper( "mov 'd0, 's0", std::list<Temp::CTemp>( 1, tmp2 ), std::list<Temp::CTemp>( 1, munchExp( exp->right.get() ) ) ) );
+		Temp::CTemp tmp3;
+		emit( new COper( "mov 'd0, 's0", std::list<Temp::CTemp>( 1, tmp3 ), std::list<Temp::CTemp>( 1, *frame->FramePointer() ) ) );
+		Temp::CTemp tmp4;
+		std::list<Temp::CTemp> source = std::list<Temp::CTemp>( 1, tmp3 );
+		source.push_back( tmp1 );
+		source.push_back( tmp2 );
+		emit( new COper( command + " 'd0, ['s0 + 's1 " + binopOperation + "'s2]",
+			std::list<Temp::CTemp>( 1, tmp4 ), source ) );
+		return tmp4;
 	}
 }
 
 Temp::CTemp CInstructionsMuncher::munchExpBinop( const IRTree::CBinop* exp )
 {
 	std::string command;
-	std::string binopOperation;
 	switch( exp->binop ) {
 		case IRTree::B_Plus:
-			command = "ADD";
-			binopOperation = "+";
+			command = "add";
 			break;
 		case IRTree::B_Minus:
-			command = "SUB";
-			binopOperation = "-";
+			command = "sub";
 			break;
 		case IRTree::B_Mul:
-			command = "MUL";
-			binopOperation = "*";
+			command = "mul";
 			break;
 		case IRTree::B_Division:
-			command = "DIV";
-			binopOperation = "/";
+			command = "div";
+			break;
+		case IRTree::B_Xor:
+			command = "xor";
 			break;
 		default:
-			// смещение только по + и -.
 			assert( false );
 	}
 	if( dynamic_cast<const IRTree::CConst*>( exp->left.get() ) != 0 && dynamic_cast<const IRTree::CConst*>( exp->right.get() ) != 0 ) {
 		int leftValue = dynamic_cast<const IRTree::CConst*>( exp->left.get() )->value;
 		int rightValue = dynamic_cast<const IRTree::CConst*>( exp->right.get() )->value;
 		Temp::CTemp tmp;
-		emit( new COper( command + " d0 <- " + std::to_string( leftValue ) + binopOperation + std::to_string( rightValue ) + "\n",
-			std::list<Temp::CTemp>( 1, tmp ), std::list<Temp::CTemp>( ) ) );
+		emit( new COper( "mov 'd0, " + std::to_string( leftValue ) + "\n", std::list<Temp::CTemp>( 1, tmp ), std::list<Temp::CTemp>() ) );
+		emit( new COper( command + " 'd0, " + std::to_string( rightValue ), std::list<Temp::CTemp>( 1, tmp ), std::list<Temp::CTemp>() ) );
 		return tmp;
 	} else if( dynamic_cast<const IRTree::CConst*>( exp->left.get() ) != 0 ) {
 		int leftValue = dynamic_cast<const IRTree::CConst*>( exp->left.get() )->value;
-		Temp::CTemp tmp;
-		emit( new COper( command + " d0 <- " + std::to_string( leftValue ) + binopOperation + "s0\n",
-			std::list<Temp::CTemp>( 1, tmp ), std::list<Temp::CTemp>( 1, munchExp( exp->right.get() ) ) ) );
-		return tmp;
+		Temp::CTemp tmp1;
+		emit( new COper( "mov 'd0, 's0", std::list<Temp::CTemp>( 1, tmp1 ), std::list<Temp::CTemp>( 1, munchExp( exp->right.get() ) ) ) );
+		Temp::CTemp tmp2;
+		emit( new COper( "mov 'd0, "  + std::to_string( leftValue ), std::list<Temp::CTemp>( 1, tmp2 ), std::list<Temp::CTemp>() ) );
+		emit( new COper( command + " 'd0, 's0", std::list<Temp::CTemp>( 1, tmp2 ), std::list<Temp::CTemp>( 1, tmp1 ) ) );
+		return tmp2;
 	} else if( dynamic_cast<const IRTree::CConst*>( exp->right.get() ) != 0 ) {
 		int rightValue = dynamic_cast<const IRTree::CConst*>( exp->right.get() )->value;
 		Temp::CTemp tmp;
-		emit( new COper( command + " d0 <- s0"+ binopOperation + std::to_string( rightValue ) + "\n",
-			std::list<Temp::CTemp>( 1, tmp ), std::list<Temp::CTemp>( 1, munchExp( exp->left.get() ) ) ) );
+		emit( new COper( "mov 'd0, 's0", std::list<Temp::CTemp>( 1, tmp ), std::list<Temp::CTemp>( 1, munchExp( exp->left.get() ) ) ) );
+		emit( new COper( command + " 'd0, " + std::to_string( rightValue ), std::list<Temp::CTemp>( 1, tmp ), std::list<Temp::CTemp>() ) );
 		return tmp;
 	} else {
-		Temp::CTemp tmp;
-		std::list<Temp::CTemp> src( 1, munchExp( exp->left.get() ) );
-		src.push_back( munchExp( exp->right.get() ) ); 
-		emit( new COper( command + " d0 <- s0" + binopOperation + "s1\n",
-			std::list<Temp::CTemp>( 1, tmp ), src ) );
-		return tmp;
+		Temp::CTemp tmp1;
+		emit( new COper( "mov 'd0, 's0", std::list<Temp::CTemp>( 1, tmp1 ), std::list<Temp::CTemp>( 1, munchExp( exp->left.get() ) ) ) );
+		Temp::CTemp tmp2;
+		emit( new COper( "mov 'd0, 's0", std::list<Temp::CTemp>( 1, tmp2 ), std::list<Temp::CTemp>( 1, munchExp( exp->right.get() ) ) ) );
+		emit( new COper( command + " 'd0, 's0",	std::list<Temp::CTemp>( 1, tmp1 ), std::list<Temp::CTemp>( 1, tmp2 ) ) );
+		return tmp1;
 	}
 }
