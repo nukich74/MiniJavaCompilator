8c4c08ae7713afc03d8a52d153ccdc4c5cb41c3b
FrolovNikolay <frolov.nikolay@phystech.edu>
2015-05-18 15:53:10 +0400

    Правки к проекту.

---
+++ Compilers/Compilers/PrologueAndEpilogue/PrologueWriter.h
@@ -0,0 +1,14 @@
+// Автор: Николай Фролов.
+
+// Описание: добавляет код пролога к функции.
+
+#pragma once
+
+#include <Frame.h>
+#include <Instruction.h>
+
+class CPrologueWriter {
+public:
+private:
+
+};
